89cea2ca871688fc3c409964d64f49f632525f80
Coralie-Roux <coralieroux.osteo@orange.fr>
2025-05-19 12:39:54 +0200

    Delete Projet_Coco_Osteo directory

--- Projet_Coco_Osteo/js/animations.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const elementsToAnimate = document.querySelectorAll('.anim-title');
-    elementsToAnimate.forEach(function(element, index) {
-        setTimeout(function() {
-            element.style.opacity = 1;
-        }, 1000 * index);
-    });
-});
--- Projet_Coco_Osteo/js/carousel.js
+++
@@ -1,9 +0,0 @@
-new Splide("#image-carousel", {
-  type: "loop",
-  heightRatio: 0.5,
-  pagination: true,
-  autoplay: true,
-  interval: 4000,
-  arrows: false,
-  easing: "ease-in-out",
-}).mount();
